d15acc3c3772f127c0e1c83a34316f317fadad72
Simao Gomes Viana <xdevs23@outlook.com>
2018-02-11 18:21:48 +0100

    msm8998-common: tweak fling velocity system props
    Change-Id: I755a802e45fd07733e7efd42ad967f5a91190bb0
    
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -105,6 +105,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.core_ctl_min_cpu=2 \
     ro.vendor.qti.core_ctl_max_cpu=4
 
+# Fling
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.min.fling_velocity=160 \
+    ro.max.fling_velocity=20000
+
 # Data
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.use_data_netmgrd=true \
