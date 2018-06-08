0a0b58f8988b2654731883db7422714b2ea64a30
Chris Craik <ccraik@google.com>
2018-06-08 13:02:29 -0700

    msm8998-common: disable use_buffer_age to workaround driver issue
    Bug: 74534157
    Test: none
    
    Change-Id: Ic2960e7c8fbb4089440accc5360929cd56cc8edb
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -142,6 +142,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.use_buffer_age=false \
     ro.sf.lcd_density=420 \
     ro.opengles.version=196610 \
     debug.sf.hw=1 \
