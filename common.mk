1cede8b454f5a7b70830dba1637ded94589a711d
NurKeinNeid <mralexman3000@gmail.com>
2018-12-02 13:09:36 +0100

    msm8998-common: AOSiP/DerpFest bringup
     * Setup makefiles and dependencies for AOSiP
     * Update extract-utils for AOSiP
     * Track PixelExperience GApps and Mods
     * Introduce Illusion Kernel

--- common.mk
+++ common.mk
@@ -142,7 +142,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    Snap
+    GoogleCameraModOP5 \
+    Playground \
+    ARStickersMod
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
