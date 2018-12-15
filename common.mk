026d9e7b371f059c74fc621bf32edc462db022ac
celtare21 <celtare21@gmail.com>
2018-12-15 09:30:48 +0000

    msm8998-common: Introduce power-libpermgr
    Based on https://github.com/wight554/device_xiaomi_msm8998-common/tree/pie/power-libperfmgr
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- common.mk
+++ common.mk
@@ -383,7 +383,10 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service.msm8998-libperfmgr
+
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Low power Whitelist
 PRODUCT_COPY_FILES += \
