0bc392f42b995ffbb961a3f6c5368da8b2e9258b
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-03-05 11:10:00 +0100

    msm8998-common: update props for wfd
    Change-Id: I3b00db0b187004815ab02250cbef51ddbcd4adf9

--- common.mk
+++ common.mk
@@ -355,7 +355,6 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_PACKAGES += \
-    libaacwrapper \
     libc2dcolorconvert \
     libextmedia_jni \
     libhypv_intercept \
@@ -517,5 +516,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wifi_concurrency_cfg.txt:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wifi_concurrency_cfg.txt
 
 # Wi-Fi Display
+PRODUCT_PACKAGES += \
+    libaacwrapper \
+    libnl
+
 PRODUCT_BOOT_JARS += \
     WfdCommon
